4e3a8723c47d1a2ac8b51da1c7d518bf730862f5
Noor <thenoorulhassan.dev@gmail.com>
2026-08-14 01:54:23 +0500

    Contains Duplicate II using hash set

--- ContainsDuplicate.II.js
+++ ContainsDuplicate.II.js
@@ -18,3 +18,20 @@ function containsNearbyDuplicate(nums, k) {
 
   return false;
 }
+
+// Using Hash set
+
+function containsNearbyDuplicate(nums, k) {
+  const window = new Set();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (window.has(nums[i])) return true;
+
+    window.add(nums[i]);
+
+    if (window.size > k) {
+      window.delete(nums[i - k]);
+    }
+  }
+  return false;
+}
